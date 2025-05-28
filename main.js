bff22968272ea74959b038f43df440a839c3bdcb
victoriia06 <k.kim221b@gmail.com>
2025-05-28 18:22:41 +0300

    Update main.js

--- main.js
+++ main.js
@@ -2,12 +2,17 @@ import { setRecaptchaTheme } from './recaptcha.js';
 import { initArrowSlider } from './arrowSlider.js';
 import { initWideSliders } from './wideSlider.js';
 import { initFormSubmission } from './formSubmission.js';
-import {mobileMenu} from './mobileMenu.js';
+import { mobileMenu } from './mobileMenu.js';
+
 document.addEventListener("DOMContentLoaded", () => {
   setRecaptchaTheme();
   initArrowSlider();
   initWideSliders();
   initFormSubmission();
   mobileMenu();
+  
+  // Добавляем проверку на отключенный JavaScript
+  if (document.getElementById('form')) {
+    document.getElementById('form').setAttribute('data-js', 'true');
+  }
 });
-
