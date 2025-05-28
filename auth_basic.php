b8b8b33a2e3eceffa147e428324f2da05aea1669
victoriia06 <k.kim221b@gmail.com>
2025-05-28 20:43:19 +0300

    Update auth_basic.php

--- auth_basic.php
+++ auth_basic.php
@@ -1,26 +1,25 @@
 <?php
-
-// auth_basic.php - HTTP Authentication Script v 1.0
-//##################################################
-
 function auth(&$request, $r) {
-  // TODO: запрашивать пользователя из БД.
-  $users = array(
-    'admin' => '123',
-  );
-  if (empty($user) && !empty($_SERVER['PHP_AUTH_USER'])) {
-    $user = array(
-      'login' => $_SERVER['PHP_AUTH_USER'],
-      'pass' => $users[$_SERVER['PHP_AUTH_USER']]
-    );
-    $request['user'] = $user;
+  if (!empty($_SESSION['user'])) {
+    $request['user'] = $_SESSION['user'];
+    return;
   }
-  if (!isset($_SERVER['PHP_AUTH_USER']) || empty($user) || $_SERVER['PHP_AUTH_USER'] != $user['login'] || $_SERVER['PHP_AUTH_PW'] != $user['pass']) {
-    unset($user);
-    $response = array(
-      'headers' => array(sprintf('WWW-Authenticate: Basic realm="%s"', conf('sitename')), 'HTTP/1.0 401 Unauthorized'),
-      'entity' => theme('401', $request),
-    );
-    return $response;
+
+  if (!empty($_SERVER['PHP_AUTH_USER'])) {
+    $user = db_row(db_query(
+      "SELECT * FROM users WHERE login = ? AND password = ?",
+      $_SERVER['PHP_AUTH_USER'],
+      $_SERVER['PHP_AUTH_PW']
+    ));
+
+    if ($user) {
+      $_SESSION['user'] = $user;
+      $request['user'] = $user;
+      return;
+    }
   }
+
+  header('WWW-Authenticate: Basic realm="Drupal Support"');
+  header('HTTP/1.0 401 Unauthorized');
+  exit;
 }
