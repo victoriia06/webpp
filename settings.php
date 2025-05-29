685ab8b501ed4f906564a40a4c731bddabc228dd
victoriia06 <k.kim221b@gmail.com>
2025-05-29 06:13:09 +0300

    Update settings.php

--- settings.php
+++ settings.php
@@ -1,7 +1,9 @@
 <?php
+// Настройки отображения ошибок
 define('DISPLAY_ERRORS', 1);
 define('INCLUDE_PATH', './api' . PATH_SEPARATOR . './modules');
 
+// Основные настройки (ЗАМЕНИТЬ НАСТРОЙКИ БД НА СВОИ!)
 $conf = array(
   'sitename' => 'Drupal Support',
   'theme' => './templates',
@@ -9,13 +11,17 @@ $conf = array(
   'clean_urls' => TRUE,
   'display_errors' => 1,
   'basedir' => '/',
-  'db_host' => 'localhost',
-  'db_name' => 'drupal_support',
-  'db_user' => 'root',
-  'db_psw' => '',
-  'admin_mail' => 'admin@drupal-coder.ru'
+  
+  // НАСТРОЙКИ БАЗЫ ДАННЫХ - ЗАМЕНИТЕ ЭТИ ДАННЫЕ НА СВОИ!
+  'db_host' => 'localhost',     // Хост БД
+  'db_name' => 'u70422', // Имя базы данных
+  'db_user' => 'u70422',          // Пользователь БД
+  'db_psw' => '4545635',               // Пароль БД
+  
+  'admin_mail' => 'c.chon98@mail.ru'
 );
 
+// Маршрутизация
 $urlconf = array(
   '' => array('module' => 'front'),
   '/^api\/submit$/' => array('module' => 'form'),
@@ -23,7 +29,10 @@ $urlconf = array(
   '/^admin$/' => array('module' => 'admin', 'auth' => 'auth_basic')
 );
 
+// Заголовки
 header('Cache-Control: no-cache, must-revalidate');
 header('Expires: Sat, 26 Jul 1997 05:00:00 GMT');
 header('Content-Type: text/html; charset=' . $conf['charset']);
+
+// Старт сессии
 session_start();
