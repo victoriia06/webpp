6551cbb4bd362cacfae9cd87a7c643f3554d476e
victoriia06 <k.kim221b@gmail.com>
2025-05-29 06:11:20 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,120 +1,67 @@
 <?php
-
+// Подключение к базе данных (использует настройки из settings.php)
 global $db;
-$db = new PDO('mysql:host=' . conf('db_host') . ';dbname=' . conf('db_name'), conf('db_user'), conf('db_psw'),
-  array(PDO::MYSQL_ATTR_FOUND_ROWS => true, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+try {
+  $db = new PDO(
+    'mysql:host=' . conf('localhost') . ';dbname=' . conf('u70422'), 
+    conf('u70422'), 
+    conf('4545635'),
+    array(
+      PDO::MYSQL_ATTR_FOUND_ROWS => true, 
+      PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    )
+  );
+} catch (PDOException $e) {
+  die("Database connection failed: " . $e->getMessage());
+}
 
+// Функция для получения одной строки
 function db_row($stmt) {
   return $stmt->fetch(PDO::FETCH_ASSOC);
 }
 
+// Функция для получения информации об ошибке
 function db_error() {
   global $db;
   return $db->errorInfo();
 }
 
+// Функция для выполнения запроса с несколькими строками
 function db_query($query) {
   global $db;
-  $q = $db->prepare($query);
   $args = func_get_args();
-  array_shift($args);
-  $res = $q->execute($args);
-  if ($res) {
-    while ($row = db_row($res)) {
-      if (isset($row['id']) && !isset($r[$row['id']])) {
-        $r[$row['id']] = $row;
-      }
-      else {
-        $r[] = $row;
-      }
-    }
-  }
-  return $r;
+  $query = array_shift($args);
+  
+  $stmt = $db->prepare($query);
+  $stmt->execute($args);
+  return $stmt;
 }
 
+// Функция для выполнения запроса с одной строкой результата
 function db_result($query) {
   global $db;
-  $q = $db->prepare($query);
   $args = func_get_args();
-  array_shift($args);
-  $res = $q->execute($args);
-  if ($res) {
-    if ($row = db_row($res)) {
-      return $row[0];
-    }
-    else {
-      return FALSE;
-    }
-  }
-  else {
-    return FALSE;
-  }
+  $query = array_shift($args);
+  
+  $stmt = $db->prepare($query);
+  $stmt->execute($args);
+  $row = $stmt->fetch(PDO::FETCH_NUM);
+  return $row ? $row[0] : FALSE;
 }
 
+// Функция для выполнения команд (INSERT, UPDATE, DELETE)
 function db_command($query) {
   global $db;
-  $q = $db->prepare($query);
   $args = func_get_args();
-  array_shift($args);
-  return $res = $q->execute($args);
+  $query = array_shift($args);
+  
+  $stmt = $db->prepare($query);
+  return $stmt->execute($args);
 }
 
+// Функция для получения последнего вставленного ID
 function db_insert_id() {
   global $db;
   return $db->lastInsertId();
-}
-
-function db_get($name, $default = FALSE) {
-  if (strlen($name) == 0) {
-    return $default;
-  }
-  $value = db_result("SELECT value FROM variable WHERE name = ?", $name);
-  if ($value === FALSE) {
-    return $default;
-  }
-  else {
-    return $value;
-  }
-}
-
-function db_set($name, $value) {
-  if (strlen($name) == 0) {
-    return;
-  }
-
-  $v = db_get($name);
-  if ($v === FALSE) {
-    $q = "INSERT INTO variable VALUES (?, ?)";
-    return db_command($q, $name, $value) > 0;
-  }
-  else {
-    $q = "UPDATE variable SET value = ? WHERE name = ?";
-    return db_command($q, $value, $name) > 0;
-  }
-}
-
-function db_sort_sql() {
-}
-
-function db_pager_query() {
-}
-
-function db_array() {
-  $args = func_get_args();
-  $key = array_shift($args);
-  $query = array_shift($args);
-  $q = $db->prepare($query);
-  $res = $q->execute($args);
-  $r = array();
-  if ($res) {
-    while ($row = db_row($res)) {
-      if (!empty($key) && isset($row[$key]) && !isset($r[$row[$key]])) {
-        $r[$row[$key]] = $row;
-      }
-      else {
-        $r[] = $row;
-      }
-    }
-  }
-  return $r;
 }
